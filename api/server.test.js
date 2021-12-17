93f3eb67f054fbc3ea51a456bd7c77e512c4bca1
Kat <kyevsukov@gmail.com>
2021-12-16 19:10:30 -0500

    wrote tests

---
+++ api/server.test.js
@@ -0,0 +1,42 @@
+const server = require('./server')
+const request = require('supertest')
+const db = require('../data/dbConfig')
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+beforeEach(async () => {
+  await db.seed.run()
+})
+afterAll(async () => {
+  await db.destroy()
+})
+
+describe('[GET] /cheese', () => {
+    test('responds with all the cheese', async () => {
+      const res = await request(server).get('/cheese')
+      expect(res.status).toBe(200)
+      expect(res.body).toHaveLength(5)
+    })
+  })
+  
+  describe('[GET] /cheese/:id', () => {
+    test('responds with gorgonzola', async () => {
+      const res = await request(server).get('/cheese/1')
+      expect(res.body).toMatchObject({ id: 1, name: 'gorgonzola' })
+    })
+  })
+  
+  describe('[POST] /cheese', () => {
+    test('responds with new cheese', async () => {
+      const res = await request(server)
+        .post('/cheese').send({ name: 'feta' })
+      expect(res.body).toMatchObject({ id: 6, name: "feta" })
+    })
+    test('responds with status 201', async () => {
+      const res = await request(server)
+        .post('/cheese').send({ name: 'feta' })
+      expect(res.status).toBe(201)
+    })
+  })
